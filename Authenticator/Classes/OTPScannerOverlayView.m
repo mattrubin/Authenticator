73b2469ae9620dfbfd747dfaf29a1f0fe3ac4bdb
Matt Rubin <mattrubin@me.com>
2013-10-14 19:20:29 -0400

    Rewritten scanner overlay with cleaner code using a clipping mask

--- Authenticator/Classes/OTPScannerOverlayView.m
+++ Authenticator/Classes/OTPScannerOverlayView.m
@@ -1,108 +1,58 @@
 //
 //  OTPScannerOverlayView.m
+//  Authenticator
 //
-//  Copyright 2013 Matt Rubin
-//  Copyright 2011 Google Inc.
+//  Copyright (c) 2013 Matt Rubin
 //
-//  Licensed under the Apache License, Version 2.0 (the "License"); you may not
-//  use this file except in compliance with the License.  You may obtain a copy
-//  of the License at
+//  Permission is hereby granted, free of charge, to any person obtaining a copy of
+//  this software and associated documentation files (the "Software"), to deal in
+//  the Software without restriction, including without limitation the rights to
+//  use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+//  the Software, and to permit persons to whom the Software is furnished to do so,
+//  subject to the following conditions:
 //
-//  http://www.apache.org/licenses/LICENSE-2.0
+//  The above copyright notice and this permission notice shall be included in all
+//  copies or substantial portions of the Software.
 //
-//  Unless required by applicable law or agreed to in writing, software
-//  distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
-//  WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the
-//  License for the specific language governing permissions and limitations under
-//  the License.
+//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+//  FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+//  COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+//  IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+//  CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 //
 
 #import "OTPScannerOverlayView.h"
 
+
 @implementation OTPScannerOverlayView
 
-- (id)initWithFrame:(CGRect)frame {
-  if ((self = [super initWithFrame:frame])) {
-    self.opaque = NO;
-    self.autoresizingMask
-      = UIViewAutoresizingFlexibleWidth | UIViewAutoresizingFlexibleHeight;
-  }
-  return self;
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        self.opaque = NO;
+        self.autoresizingMask = UIViewAutoresizingFlexibleWidth | UIViewAutoresizingFlexibleHeight;
+    }
+    return self;
 }
 
-- (void)drawRect:(CGRect)rect {
-  CGContextRef context = UIGraphicsGetCurrentContext();
-  CGRect bounds = self.bounds;
-  CGFloat rectHeight = 200;
-  CGFloat oneSixthRectHeight = rectHeight * 0.165f;
-  CGFloat midX = CGRectGetMidX(bounds);
-  CGFloat midY = CGRectGetMidY(bounds);
-  CGFloat minY = CGRectGetMinY(bounds);
-  CGFloat minX = CGRectGetMinX(bounds);
-  CGFloat maxY = CGRectGetMaxY(bounds);
-  CGFloat maxX = CGRectGetMaxX(bounds);
+- (void)drawRect:(CGRect)rect
+{
+    CGContextRef context = UIGraphicsGetCurrentContext();
 
-  // Blackout boxes
-  CGRect scanRect = CGRectMake(midX - rectHeight * .5f,
-                               midY - rectHeight * .5f,
-                               rectHeight,
-                               rectHeight);
-  CGRect leftRect = CGRectMake(minX, minY,
-                               CGRectGetMinX(scanRect), maxY);
-  CGRect rightRect = CGRectMake(CGRectGetMaxX(scanRect), minY,
-                                maxX - CGRectGetMaxX(scanRect), maxY);
-  CGRect bottomRect = CGRectMake(minX, minY,
-                                 maxX, CGRectGetMinY(scanRect));
-  CGRect topRect = CGRectMake(CGRectGetMinX(scanRect), CGRectGetMaxY(scanRect),
-                              maxX, maxY - CGRectGetMaxY(scanRect));
-  CGContextBeginPath(context);
-  CGContextAddRect(context, leftRect);
-  CGContextAddRect(context, rightRect);
-  CGContextAddRect(context, bottomRect);
-  CGContextAddRect(context, topRect);
-  [[[UIColor blackColor] colorWithAlphaComponent:0.3f] set];
-  CGContextFillPath(context);
+    [[UIColor colorWithWhite:0 alpha:0.5] setFill];
 
-  // Frame Box
-  [[UIColor greenColor] set];
-  midX = CGRectGetMidX(scanRect);
-  midY = CGRectGetMidY(scanRect);
-  minY = CGRectGetMinY(scanRect);
-  minX = CGRectGetMinX(scanRect);
-  maxY = CGRectGetMaxY(scanRect);
-  maxX = CGRectGetMaxX(scanRect);
-  CGContextSetLineWidth(context, 2);
-  CGContextMoveToPoint(context, midX - oneSixthRectHeight, minY);
-  CGContextAddLineToPoint(context, minX, minY);
-  CGContextAddLineToPoint(context, minX, midY - oneSixthRectHeight);
-  CGContextStrokePath(context);
-  CGContextMoveToPoint(context, minX, midY + oneSixthRectHeight);
-  CGContextAddLineToPoint(context, minX, maxY);
-  CGContextAddLineToPoint(context, midX - oneSixthRectHeight, maxY);
-  CGContextStrokePath(context);
-  CGContextMoveToPoint(context, midX + oneSixthRectHeight, maxY);
-  CGContextAddLineToPoint(context, maxX, maxY);
-  CGContextAddLineToPoint(context, maxX, midY + oneSixthRectHeight);
-  CGContextStrokePath(context);
-  CGContextMoveToPoint(context, maxX, midY - oneSixthRectHeight);
-  CGContextAddLineToPoint(context, maxX, minY);
-  CGContextAddLineToPoint(context, midX + oneSixthRectHeight, minY);
-  CGContextStrokePath(context);
+    CGFloat smallestDimension = fminf(self.bounds.size.width, self.bounds.size.height);
+    CGFloat windowSize = 0.9f * smallestDimension;
 
-  // Cross hairs
-  CGContextSetLineWidth(context, 1);
-  CGContextMoveToPoint(context, midX, minY - oneSixthRectHeight);
-  CGContextAddLineToPoint(context, midX, minY + oneSixthRectHeight);
-  CGContextStrokePath(context);
-  CGContextMoveToPoint(context, midX, maxY - oneSixthRectHeight);
-  CGContextAddLineToPoint(context, midX, maxY + oneSixthRectHeight);
-  CGContextStrokePath(context);
-  CGContextMoveToPoint(context, minX - oneSixthRectHeight, midY);
-  CGContextAddLineToPoint(context, minX + oneSixthRectHeight, midY);
-  CGContextStrokePath(context);
-  CGContextMoveToPoint(context, maxX - oneSixthRectHeight, midY);
-  CGContextAddLineToPoint(context, maxX + oneSixthRectHeight, midY);
-  CGContextStrokePath(context);
+    CGContextAddRect(context, rect);
+    CGContextAddRect(context, CGRectMake(CGRectGetMidX(rect) - windowSize/2,
+                                         CGRectGetMidY(rect) - windowSize/2,
+                                         windowSize,
+                                         windowSize));
+    CGContextEOClip(context);
+    CGContextFillRect(context, rect);
 }
 
 @end
