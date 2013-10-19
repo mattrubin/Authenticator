fb88cc6984f6fe4a92178387f38868e70fa8ddba
Matt Rubin <mattrubin@me.com>
2013-10-19 01:38:08 -0400

    Add unit testing bundle

---
+++ AuthenticatorTests/AuthenticatorTests.m
@@ -0,0 +1,34 @@
+//
+//  AuthenticatorTests.m
+//  AuthenticatorTests
+//
+//  Created by Matt Rubin on 10/19/13.
+//  Copyright (c) 2013 Matt Rubin. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface AuthenticatorTests : XCTestCase
+
+@end
+
+@implementation AuthenticatorTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
