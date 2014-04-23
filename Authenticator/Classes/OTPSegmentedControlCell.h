a537c34ed56aabeff0097d42b82a6756b35cdfbf
Matt Rubin <opensource@mattrubin.me>
2014-04-22 21:41:16 -0400

    Fix copyright date on new files

--- Authenticator/Classes/OTPSegmentedControlCell.h
+++ Authenticator/Classes/OTPSegmentedControlCell.h
@@ -2,7 +2,7 @@
 //  OTPSegmentedControlCell.h
 //  Authenticator
 //
-//  Copyright (c) 2013 Matt Rubin
+//  Copyright (c) 2014 Matt Rubin
 //
 //  Permission is hereby granted, free of charge, to any person obtaining a copy of
 //  this software and associated documentation files (the "Software"), to deal in
