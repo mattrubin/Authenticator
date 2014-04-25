3d6168c9756736e837852a4cafef8c0670323e52
Matt Rubin <opensource@mattrubin.me>
2014-04-25 06:05:58 -0400

    Organize icon source files

--- Authenticator/Resources/GenerateIcons.sh
+++ Authenticator/Resources/GenerateIcons.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 # Generate App Icons
-inkscape -z -e "Icon-29@2x.png" -w 58 "icon.svg";
-inkscape -z -e "Icon-40@2x.png" -w 80 "icon.svg";
-inkscape -z -e "Icon-60@2x.png" -w 120 "icon.svg";
+inkscape -z -e "Icon-29@2x.png" -w 58 "Icon.svg";
+inkscape -z -e "Icon-40@2x.png" -w 80 "Icon.svg";
+inkscape -z -e "Icon-60@2x.png" -w 120 "Icon.svg";
 
 for file in Icon-*.png; do
   pngcrush -rem alla -ow -res 144 "$file" "_$file";
@@ -12,8 +12,8 @@ done
 mv Icon-*.png "Images.xcassets/AppIcon.appiconset/"
 
 # Generate iTunes Artwork
-inkscape -z -e "iTunesArtwork" -w 512 "icon.svg";
-inkscape -z -e "iTunesArtwork@2x" -w 1024 "icon.svg";
+inkscape -z -e "iTunesArtwork" -w 512 "Icon.svg";
+inkscape -z -e "iTunesArtwork@2x" -w 1024 "Icon.svg";
 
 pngcrush -rem alla -ow -res 144 "iTunesArtwork@2x" "_iTunesArtwork@2x";
 pngcrush -rem alla -ow "iTunesArtwork" "_iTunesArtwork";
