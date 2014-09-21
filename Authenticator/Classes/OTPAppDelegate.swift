c089e75caec1995e3a6e8e601da10f6c1c6f999d
Matt Rubin <opensource@mattrubin.me>
2014-09-21 18:26:42 -0400

    Delete main.m

--- Authenticator/Classes/OTPAppDelegate.swift
+++ Authenticator/Classes/OTPAppDelegate.swift
@@ -24,6 +24,7 @@
 
 import UIKit
 
+@UIApplicationMain
 class OTPAppDelegate: UIResponder, UIApplicationDelegate {
     let window = UIWindow(frame: UIScreen.mainScreen().bounds)
     let rootViewController = OTPTokenListViewController()
