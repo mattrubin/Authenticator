e137366276d050ccc2b8b5a82ec802a05ab6b256
Matt Rubin <opensource@mattrubin.me>
2015-09-14 12:44:24 -0400

    Clean up TableViewModel

--- Authenticator/Classes/TokenForm.swift
+++ Authenticator/Classes/TokenForm.swift
@@ -9,6 +9,15 @@
 import OneTimePasswordLegacy
 
 @objc
+protocol TableViewModel {
+    var title: String { get }
+    var numberOfSections: Int { get }
+    func numberOfRowsInSection(section: Int) -> Int
+    func cellForRowAtIndexPath(indexPath: NSIndexPath) -> UITableViewCell?
+    func viewForHeaderInSection(section:Int) -> UIView?
+}
+
+@objc
 protocol TokenForm: TableViewModel {
     weak var presenter: TokenFormPresenter? { get set }
 
