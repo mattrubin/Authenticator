65e75e12f92a40adaaae382a4ea17b66730e8ea6
Matt Rubin <opensource@mattrubin.me>
2015-09-15 23:15:33 -0400

    Remove OTPTokenFormViewController

--- Authenticator/Classes/TokenFormViewController.swift
+++ Authenticator/Classes/TokenFormViewController.swift
@@ -22,7 +22,7 @@
 //  CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 //
 
-class TokenFormViewController: OTPTokenFormViewController {
+class TokenFormViewController: UITableViewController {
     var form: TokenForm?
     var doneButtonItem: UIBarButtonItem?
 
