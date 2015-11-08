023588db13b1c74bb85e677b4b26ae86ec5fd020
Matt Rubin <opensource@mattrubin.me>
2015-11-07 21:40:02 -0500

    Clean up Scanner

--- Authenticator/Classes/Scanner.swift
+++ Authenticator/Classes/Scanner.swift
@@ -2,8 +2,24 @@
 //  Scanner.swift
 //  Authenticator
 //
-//  Created by Matt Rubin on 11/7/15.
-//  Copyright © 2015 Matt Rubin. All rights reserved.
+//  Copyright (c) 2015 Matt Rubin
+//
+//  Permission is hereby granted, free of charge, to any person obtaining a copy of
+//  this software and associated documentation files (the "Software"), to deal in
+//  the Software without restriction, including without limitation the rights to
+//  use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+//  the Software, and to permit persons to whom the Software is furnished to do so,
+//  subject to the following conditions:
+//
+//  The above copyright notice and this permission notice shall be included in all
+//  copies or substantial portions of the Software.
+//
+//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+//  FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+//  COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+//  IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+//  CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 //
 
 import AVFoundation
@@ -14,14 +30,14 @@ protocol ScannerDelegate {
     func handleError(error: NSError)
 }
 
-class Scanner: NSObject {
+class Scanner: NSObject, AVCaptureMetadataOutputObjectsDelegate {
     class var deviceCanScan: Bool {
         return (AVCaptureDevice.defaultDeviceWithMediaType(AVMediaTypeVideo) != nil)
     }
 
     weak var delegate: ScannerDelegate?
     private var captureSession: AVCaptureSession?
-    let serialQueue = dispatch_queue_create("QR Scanner serial queue", DISPATCH_QUEUE_SERIAL);
+    private let serialQueue = dispatch_queue_create("QR Scanner serial queue", DISPATCH_QUEUE_SERIAL);
 
     init(delegate: ScannerDelegate) {
         self.delegate = delegate
@@ -55,6 +71,11 @@ class Scanner: NSObject {
         }
     }
 
+    enum CaptureSessionError: ErrorType {
+        case InputError
+        case OutputError
+    }
+
     private func createCaptureSession() throws -> AVCaptureSession {
         let captureSession = AVCaptureSession()
 
@@ -75,13 +96,8 @@ class Scanner: NSObject {
         return captureSession
     }
 
-    enum CaptureSessionError: ErrorType {
-        case InputError
-        case OutputError
-    }
-}
+    // MARK: AVCaptureMetadataOutputObjectsDelegate
 
-extension Scanner: AVCaptureMetadataOutputObjectsDelegate {
     func captureOutput(captureOutput: AVCaptureOutput?, didOutputMetadataObjects metadataObjects: [AnyObject]?, fromConnection connection: AVCaptureConnection?) {
         guard let metadataObjects = metadataObjects
             else {return }
