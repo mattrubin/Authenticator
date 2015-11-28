1020ad69428b027d577d9e0845719c86b84c1095
Matt Rubin <opensource@mattrubin.me>
2015-11-28 00:25:26 -0500

    Clean up diff indentation

--- Authenticator/Source/TableDiff.swift
+++ Authenticator/Source/TableDiff.swift
@@ -33,64 +33,69 @@ enum Change {
 }
 
 func diff<Row>(from oldArray: [Row], to newArray: [Row], comparator isSameRow: (Row, Row) -> Bool)
-    -> [Change] {
-        return changes(from: ArraySlice(oldArray), to: ArraySlice(newArray), comparator: isSameRow)
+    -> [Change]
+{
+    return changes(from: ArraySlice(oldArray), to: ArraySlice(newArray), comparator: isSameRow)
 }
 
 private func changes<Row>(from oldRows: ArraySlice<Row>, to newRows: ArraySlice<Row>,
-    comparator isSameRow: (Row, Row) -> Bool) -> [Change] {
-        // Work from the end to preserve earlier indices when recursing
-        switch (oldRows.last, newRows.last) {
-        case let (.Some(oldRow), .Some(newRow)):
-            if isSameRow(oldRow, newRow) {
-                // The old and new rows are the same row, and can be represented by an Update
-                // TODO: Don't update if the rows are truly equal
-                return changesWithUpdate(from: oldRows, to: newRows, comparator: isSameRow)
-            } else {
-                // The old and new rows are different, so compute the two possible change sets:
-                // one where the old row is deleted, another where the new row is inserted
-                let changesA = changesWithInsertion(from: oldRows, to: newRows, comparator: isSameRow)
-                let changesB = changesWithDeletion(from: oldRows, to: newRows, comparator: isSameRow)
+    comparator isSameRow: (Row, Row) -> Bool) -> [Change]
+{
+    // Work from the end to preserve earlier indices when recursing
+    switch (oldRows.last, newRows.last) {
+    case let (.Some(oldRow), .Some(newRow)):
+        if isSameRow(oldRow, newRow) {
+            // The old and new rows are the same row, and can be represented by an Update
+            // TODO: Don't update if the rows are truly equal
+            return changesWithUpdate(from: oldRows, to: newRows, comparator: isSameRow)
+        } else {
+            // The old and new rows are different, so compute the two possible change sets:
+            // one where the old row is deleted, another where the new row is inserted
+            let changesA = changesWithInsertion(from: oldRows, to: newRows, comparator: isSameRow)
+            let changesB = changesWithDeletion(from: oldRows, to: newRows, comparator: isSameRow)
 
-                // Return the shorter of the two change sets
-                return changesA.count < changesB.count ? changesA : changesB
-            }
-
-        case (.Some, .None):
-            // Only old rows remain, which must be deleted
-            return changesWithDeletion(from: oldRows, to: newRows, comparator: isSameRow)
-
-        case (.None, .Some):
-            // Only new rows remain, which must be inserted
-            return changesWithInsertion(from: oldRows, to: newRows, comparator: isSameRow)
-
-        case (.None, .None):
-            // All rows are accounted for
-            return []
+            // Return the shorter of the two change sets
+            return changesA.count < changesB.count ? changesA : changesB
         }
+
+    case (.Some, .None):
+        // Only old rows remain, which must be deleted
+        return changesWithDeletion(from: oldRows, to: newRows, comparator: isSameRow)
+
+    case (.None, .Some):
+        // Only new rows remain, which must be inserted
+        return changesWithInsertion(from: oldRows, to: newRows, comparator: isSameRow)
+
+    case (.None, .None):
+        // All rows are accounted for
+        return []
+    }
 }
 
 private func changesWithInsertion<Row>(from oldRows: ArraySlice<Row>, to newRows: ArraySlice<Row>,
-    comparator isSameRow: (Row, Row) -> Bool) -> [Change] {
-        let insertion = Change.Insert(index: newRows.endIndex.predecessor())
-        let changesAfterInsertion = changes(from: oldRows, to: newRows.dropLast(),
-            comparator: isSameRow)
-        return [insertion] + changesAfterInsertion
+    comparator isSameRow: (Row, Row) -> Bool) -> [Change]
+{
+    let insertion = Change.Insert(index: newRows.endIndex.predecessor())
+    let changesAfterInsertion = changes(from: oldRows, to: newRows.dropLast(),
+        comparator: isSameRow)
+    return [insertion] + changesAfterInsertion
 }
 
 private func changesWithUpdate<Row>(from oldRows: ArraySlice<Row>, to newRows: ArraySlice<Row>,
-    comparator isSameRow: (Row, Row) -> Bool) -> [Change] {
-        // TODO: Test update indices
-        let update = Change.Update(index: newRows.endIndex.predecessor())
-        let changesAfterUpdate = changes(from: oldRows.dropLast(), to: newRows.dropLast(),
-            comparator: isSameRow)
-        return [update] + changesAfterUpdate
+    comparator isSameRow: (Row, Row) -> Bool) -> [Change]
+{
+    // TODO: Test update indices
+    let update = Change.Update(index: newRows.endIndex.predecessor())
+    let changesAfterUpdate = changes(from: oldRows.dropLast(), to: newRows.dropLast(),
+        comparator: isSameRow)
+    return [update] + changesAfterUpdate
 }
 
 private func changesWithDeletion<Row>(from oldRows: ArraySlice<Row>, to newRows: ArraySlice<Row>,
-    comparator isSameRow: (Row, Row) -> Bool) -> [Change] {
-        let deletion = Change.Delete(index: oldRows.endIndex.predecessor())
-        let changesAfterDeletion = changes(from: oldRows.dropLast(), to: newRows,
-            comparator: isSameRow)
-        return [deletion] + changesAfterDeletion
+    comparator isSameRow: (Row, Row) -> Bool) -> [Change]
+{
+    let deletion = Change.Delete(index: oldRows.endIndex.predecessor())
+    let changesAfterDeletion = changes(from: oldRows.dropLast(), to: newRows,
+        comparator: isSameRow)
+    return [deletion] + changesAfterDeletion
 }
