8e59ad7865f37aa2622ec07d0f5757a80809bf4f
Matt Rubin <opensource@mattrubin.me>
2015-12-02 10:07:25 -0500

    Return correct set of changes from new diff algorithm

--- Authenticator/Source/TableDiff.swift
+++ Authenticator/Source/TableDiff.swift
@@ -51,38 +51,35 @@ private func changes<Row>(from oldRows: ArraySlice<Row>, to newRows: ArraySlice<
     comparator rowsHaveSameIdentity: (Row, Row) -> Bool, equation rowsAreEqual: (Row, Row) -> Bool)
     -> [Change]
 {
-    print("OLD: \(oldRows)")
-    print("NEW: \(newRows)")
     let MAX = oldRows.count + newRows.count
     if MAX == 0 {
         return []
     }
-    print("\(MAX) = \(oldRows.count) + \(newRows.count)")
-    var V = Array(count: (2 * MAX + 1), repeatedValue: 0)
+    var V: [(Int, [Change])] = Array(count: (2 * MAX + 1), repeatedValue: (0, []))
     for D in 0...MAX {
-        print("Finding \(D)-change paths:")
         for k in (-D).stride(through: D, by: 2) {
-            print("  in diagonal \(k)")
             var x: Int
-            if k == -D || (k != D && V[k-1 + MAX] < V[k+1 + MAX]) {
-                x = V[k+1 + MAX]
-                print("    the best predecessor comes from diagonal \(k+1)")
-                print("    which reached point (\(x),\(x-k-1)))")
+            var changes: [Change]
+            if k == -D || (k != D && V[k-1 + MAX].0 < V[k+1 + MAX].0) {
+                (x, changes) = V[k+1 + MAX]
+                if D != 0 {
+                    changes = changes + [.Insert(index: x-k - 1)]
+                }
             } else {
-                x = V[k-1 + MAX] + 1
-                print("    the best predecessor comes from diagonal \(k-1)")
-                print("    which reached point (\(x-1),\(x-k)))")
+                (x, changes) = V[k-1 + MAX]
+                x = x + 1
+                changes = changes + [.Delete(index: x - 1)]
             }
             var y = x - k
-            print("    so with one step we can reach point (\(x),\(y)))")
             while x < oldRows.count && y < newRows.count && rowsHaveSameIdentity(oldRows[x+1 - 1], newRows[y+1 - 1]) {
+                if !rowsAreEqual(oldRows[x+1 - 1], newRows[y+1 - 1]) {
+                    changes = changes + [.Update(index: x)]
+                }
                 (x, y) = (x+1, y+1)
-                print("      which can slide to (\(x),\(y)))")
             }
-            V[k + MAX] = x
+            V[k + MAX] = (x, changes)
             if x>=oldRows.count && y>=newRows.count {
-                print("       taking us to our goal, with a \(D)-change path")
-                return oldRows.indices.map({ .Delete(index: $0) }) + newRows.indices.map({ .Insert(index: $0) })
+                return changes
             }
         }
     }
