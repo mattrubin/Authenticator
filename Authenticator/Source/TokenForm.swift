d28051c12574c72caaaec1accc7669d0a3569a76
Matt Rubin <opensource@mattrubin.me>
2015-12-06 23:20:50 -0500

    Remove unnecessary import

--- Authenticator/Source/TokenForm.swift
+++ Authenticator/Source/TokenForm.swift
@@ -22,8 +22,6 @@
 //  CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 //
 
-import OneTimePassword
-
 protocol TokenForm {
     var viewModel: TableViewModel<Form> { get }
     weak var presenter: TokenFormPresenter? { get set }
