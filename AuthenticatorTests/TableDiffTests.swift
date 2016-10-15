527f2f0197cb506bf078ddceb181ac77d89e119b
Robert Collins <beaucollins@gmail.com>
2016-10-15 09:35:09 -0700

    Tests that a diff with same PersistentToken row produces no changes

--- AuthenticatorTests/TableDiffTests.swift
+++ AuthenticatorTests/TableDiffTests.swift
@@ -1,28 +1,68 @@
 //
 //  TableDiff.swift
-//  
+//  Authenticator
 //
-//  Created by Beau Collins on 10/15/16.
+//  Copyright (c) 2016 Authenticator authors
 //
+//  Permission is hereby granted, free of charge, to any person obtaining a copy
+//  of this software and associated documentation files (the "Software"), to deal
+//  in the Software without restriction, including without limitation the rights
+//  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//  copies of the Software, and to permit persons to whom the Software is
+//  furnished to do so, subject to the following conditions:
+//
+//  The above copyright notice and this permission notice shall be included in all
+//  copies or substantial portions of the Software.
+//
+//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+//  SOFTWARE.
 //
 
 import XCTest
+import OneTimePassword
 
 class TableDiffTests: XCTestCase {
-    
-    override func setUp() {
-        super.setUp()
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+
+    func testNoChanges() throws {
+        let generator = Generator(factor: .Timer(period: 60),
+                                  secret: "secret".dataUsingEncoding(NSUTF8StringEncoding)!,
+                                  algorithm: .SHA256,
+                                  digits: 6)!
+        let token = Token(name: "Token Name",
+                          issuer: "Token Issuer",
+                          generator: generator)
+        let date = NSDate()
+        let before = [
+            TokenRowModel(
+                persistentToken: try PersistentToken(token: token),
+                displayTime: DisplayTime(date: date)
+            )
+        ]
+        let after = [
+            TokenRowModel(
+                persistentToken: try PersistentToken(token: token),
+                displayTime: DisplayTime(date: date)
+            )
+        ]
+
+        let changes = changesFrom(before, to: after)
+        XCTAssertEqual(changes.count, 0)
     }
-    
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-        super.tearDown()
+
+}
+
+extension PersistentToken {
+    enum TokenError: ErrorType {
+        case IdentifierMissing
     }
-    
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+
+    init(token: Token) throws {
+        self.token = token
+        identifier = NSData()
     }
-    
 }
