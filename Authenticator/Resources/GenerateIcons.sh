90eaae80bc40281ecd4d8ec182c45bc748957074
Matt Rubin <opensource@mattrubin.me>
2017-01-03 19:40:31 -0500

    Fix icon generation
    Inkscape has a bug which requires absolute paths:
    https://bugs.launchpad.net/inkscape/+bug/1449251

--- Authenticator/Resources/GenerateIcons.sh
+++ Authenticator/Resources/GenerateIcons.sh
@@ -1,21 +1,24 @@
 #!/bin/bash
 
+PREFIX="${SRCROOT}/Authenticator/Resources"
+ICON="${PREFIX}/Icon.svg"
+
 hash inkscape 2>/dev/null || { echo >&2 "Icon generation requires inkscape. Using existing icons instead."; exit 0; }
 hash pngcrush 2>/dev/null || { echo >&2 "Icon generation requires pngcrush. Using existing icons instead."; exit 0; }
 
 # Generate App Icons
-inkscape -z -e "Icon-20.png" -w 20 "Icon.svg";
-inkscape -z -e "Icon-29.png" -w 29 "Icon.svg";
-inkscape -z -e "Icon-40.png" -w 40 "Icon.svg";
-inkscape -z -e "Icon-58.png" -w 58 "Icon.svg";
-inkscape -z -e "Icon-60.png" -w 60 "Icon.svg";
-inkscape -z -e "Icon-76.png" -w 76 "Icon.svg";
-inkscape -z -e "Icon-80.png" -w 80 "Icon.svg";
-inkscape -z -e "Icon-87.png" -w 87 "Icon.svg";
-inkscape -z -e "Icon-120.png" -w 120 "Icon.svg";
-inkscape -z -e "Icon-152.png" -w 152 "Icon.svg";
-inkscape -z -e "Icon-167.png" -w 167 "Icon.svg";
-inkscape -z -e "Icon-180.png" -w 180 "Icon.svg";
+inkscape -z -e "${PREFIX}/Icon-20.png" -w 20 $ICON;
+inkscape -z -e "${PREFIX}/Icon-29.png" -w 29 $ICON;
+inkscape -z -e "${PREFIX}/Icon-40.png" -w 40 $ICON;
+inkscape -z -e "${PREFIX}/Icon-58.png" -w 58 $ICON;
+inkscape -z -e "${PREFIX}/Icon-60.png" -w 60 $ICON;
+inkscape -z -e "${PREFIX}/Icon-76.png" -w 76 $ICON;
+inkscape -z -e "${PREFIX}/Icon-80.png" -w 80 $ICON;
+inkscape -z -e "${PREFIX}/Icon-87.png" -w 87 $ICON;
+inkscape -z -e "${PREFIX}/Icon-120.png" -w 120 $ICON;
+inkscape -z -e "${PREFIX}/Icon-152.png" -w 152 $ICON;
+inkscape -z -e "${PREFIX}/Icon-167.png" -w 167 $ICON;
+inkscape -z -e "${PREFIX}/Icon-180.png" -w 180 $ICON;
 
 for file in Icon-*.png; do
   pngcrush -rem alla -ow -res 144 "$file" "_$file";
@@ -24,8 +27,8 @@ done
 mv Icon-*.png "Images.xcassets/AppIcon.appiconset/"
 
 # Generate iTunes Artwork
-inkscape -z -e "iTunesArtwork" -w 512 "Icon.svg";
-inkscape -z -e "iTunesArtwork@2x" -w 1024 "Icon.svg";
+inkscape -z -e "${PREFIX}/iTunesArtwork" -w 512 $ICON;
+inkscape -z -e "${PREFIX}/iTunesArtwork@2x" -w 1024 $ICON;
 
 pngcrush -rem alla -ow -res 144 "iTunesArtwork@2x" "_iTunesArtwork@2x";
 pngcrush -rem alla -ow "iTunesArtwork" "_iTunesArtwork";
