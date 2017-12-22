c96e4453186742db8c9e9334e991430d50f1af44
Matt Rubin <opensource@mattrubin.me>
2017-12-21 21:17:34 -0500

    Clean up common initialization in RingLayer

--- Authenticator/Source/ProgressRingView.swift
+++ Authenticator/Source/ProgressRingView.swift
@@ -97,18 +97,22 @@ class ProgressRingView: UIView {
 private class RingLayer: CAShapeLayer {
     override init() {
         super.init()
-        lineWidth = 1.5
-        fillColor = nil
+        setPropertyDefaults()
     }
 
     override init(layer: Any) {
         super.init(layer: layer)
-        lineWidth = 1.5
-        fillColor = nil
+        setPropertyDefaults()
     }
 
     required init?(coder aDecoder: NSCoder) {
         super.init(coder: aDecoder)
+        setPropertyDefaults()
+    }
+
+    private func setPropertyDefaults() {
+        lineWidth = 1.5
+        fillColor = nil
     }
 
     override func layoutSublayers() {
