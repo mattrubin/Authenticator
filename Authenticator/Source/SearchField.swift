1fdf766179a4e99b1eb5b47190440aefc08e3c1e
Matt Rubin <opensource@mattrubin.me>
2019-08-23 21:24:46 -0400

    Update copyright dates

--- Authenticator/Source/SearchField.swift
+++ Authenticator/Source/SearchField.swift
@@ -2,7 +2,7 @@
 //  SearchField.swift
 //  Authenticator
 //
-//  Copyright (c) 2013-2017 Authenticator authors
+//  Copyright (c) 2013-2019 Authenticator authors
 //
 //  Permission is hereby granted, free of charge, to any person obtaining a copy
 //  of this software and associated documentation files (the "Software"), to deal
--- Authenticator/Source/TokenFormViewController.swift
+++ Authenticator/Source/TokenFormViewController.swift
@@ -2,7 +2,7 @@
 //  TokenFormViewController.swift
 //  Authenticator
 //
-//  Copyright (c) 2015-2018 Authenticator authors
+//  Copyright (c) 2015-2019 Authenticator authors
 //
 //  Permission is hereby granted, free of charge, to any person obtaining a copy
 //  of this software and associated documentation files (the "Software"), to deal
