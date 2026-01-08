f7bc289ebc75b42874958e1f4de9abeca223d594
Matt Rubin <opensource@mattrubin.me>
2026-01-07 23:18:03 -0500

    Hide the "Send error report" button if the iOS mail app is not set up

--- Authenticator/Source/ErrorViewController.swift
+++ Authenticator/Source/ErrorViewController.swift
@@ -2,7 +2,7 @@
 //  ErrorViewController.swift
 //  Authenticator
 //
-//  Copyright (c) 2023 Authenticator authors
+//  Copyright (c) 2023-2026 Authenticator authors
 //
 //  Permission is hereby granted, free of charge, to any person obtaining a copy
 //  of this software and associated documentation files (the "Software"), to deal
@@ -85,6 +85,8 @@ class ErrorViewController: UIViewController {
 
         let errorReportString = errorReport.toString()
         label.text = errorReportString
+
+        emailButton.isHidden = !MFMailComposeViewController.canSendMail()
     }
 
     @available(*, unavailable)
